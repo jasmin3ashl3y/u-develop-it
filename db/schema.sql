9c775a1b043b24df4f174c6122417185da74e2ea
jasmin3ashl3y <jasmine@ashleyit.com>
2021-11-28 23:33:07 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,18 @@ CREATE TABLE candidates (
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE votes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
